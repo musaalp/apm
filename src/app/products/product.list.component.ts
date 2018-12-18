44d2ee6676dc50a60d6fd7eaccaa997312dc4151
Musa Alp <musaa.alp@gmail.com>
2018-12-18 14:08:31 +0300

    implement product service with hardcoded data

--- src/app/products/product.list.component.ts
+++ src/app/products/product.list.component.ts
@@ -1,18 +1,20 @@
 
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './product.service';
 
 @Component({
     selector: 'pm-products',
     templateUrl: './product-list.component.html',
-    styleUrls: ['./product-list.component.css']
+    styleUrls: ['./product-list.component.css'],
+    providers: [ProductService]
 })
 
 export class ProductListComponent implements OnInit {
 
-    constructor() {
-        this.filteredProducts = this.products;
-        this._listFilter = "cart";
+    private _productService;
+    constructor(productService: ProductService) {
+        this._productService = productService;
     }
 
     pageTitle: string = "Product List";
@@ -31,36 +33,15 @@ export class ProductListComponent implements OnInit {
     }
 
     filteredProducts: IProduct[];
-    products: IProduct[] =
-        [
-            {
-                "productId": 1,
-                "productName": "Leaf Rake",
-                "productCode": "GDN-0011",
-                "releaseDate": "March 19, 2016",
-                "description": "Leaf rake with 48-inch wooden handle.",
-                "price": 19.95,
-                "starRating": 3.2,
-                "imageUrl": "https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-            },
-            {
-                "productId": 2,
-                "productName": "Garden Cart",
-                "productCode": "GDN-0023",
-                "releaseDate": "March 18, 2016",
-                "description": "15 gallon capacity rolling garden cart",
-                "price": 32.99,
-                "starRating": 4.2,
-                "imageUrl": "https://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-            }
-        ];
+    products: IProduct[];    
 
     toggleImage(): void {
         this.showImage = !this.showImage;
     }
 
     ngOnInit(): void {
-        console.log("In OnInit")
+        this.products = this._productService.getProducts();        
+        this.filteredProducts = this.products;
     }
 
     performFilter(filterBy: string): IProduct[] {
